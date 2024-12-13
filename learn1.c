81068f61ae240f21456e50fa0042e7c6d2311bb1
varun-arya-saini <varunarya729@gmail.com>
2024-12-13 13:49:18 +0530

    program for linear search

--- learn1.c
+++ learn1.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
-void main(){
-    int a;
-    printf("enter os name\n");
-    scanf("%d",&a); 
-    printf("\n\n\n\nOS name is :");
-    getch();
+int main(){
+    int a[100], i,n,x;
+    printf("Enter the size\n");
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    scanf("%d",&a[i]);
+    printf("\n Enter element to be searched\n");
+    scanf("%d",&x);
+    for(i=0; i<n; i++){
+        if(a[i]==x){
+            printf("\n Element found at%d position", i+1);
+            break;  
+        }
+    }
+    if(i==n){
+        printf("\n Element not found");
+    }
+
+    return;
 }
 
